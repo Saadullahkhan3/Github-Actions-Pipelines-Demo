f8a3c66d1fdf7a8bba09df9d64afc10dfd732b2d
Saadullah Khan <Saadullahkhan43251@gmail.com>
2025-09-10 00:48:35 +0500

    fix: assign repo variables as env vars in workflow as bash can't directly access them

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-echo "0 - Hello World from ${ vars.YOUR_NAME } and ${ vars.MY_NAME }"
-echo "1 - Hello World from ${YOUR_NAME} and ${MY_NAME}"
-echo "2 - Hello World from $YOUR_NAME and $MY_NAME"
+echo "Hello World from ${ YOUR_NAME } and ${ MY_NAME }"
