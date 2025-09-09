5fa41f35e76773c99a2a33b82375033a3c59d9cd
Saadullah Khan <Saadullahkhan43251@gmail.com>
2025-09-10 00:37:37 +0500

    trying to access github action variables in shell

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-echo "Hello World from ${ vars.YOUR_NAME } and ${ vars.MY_NAME }"
-echo "Hello World from ${YOUR_NAME} and ${MY_NAME}"
+echo "0 - Hello World from ${ vars.YOUR_NAME } and ${ vars.MY_NAME }"
+echo "1 - Hello World from ${YOUR_NAME} and ${MY_NAME}"
+echo "2 - Hello World from $YOUR_NAME and $MY_NAME"
